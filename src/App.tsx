674df8c862c00d2cb16063602b636fd835642393
dev001hajipro <dev001hajipro@gmail.com>
2022-02-17 18:11:07 +0900

    sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Box, Button, IconButton, Link, Toolbar, Typography } from '@mui/material';
+
+import MenuIcon from '@mui/icons-material/Menu'
+import { Link as RouterLink, Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
+import { Product } from './components/Product';
+import { Order } from './components/Order';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            注文
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <div>コンポーネントの切り替えがしたい。</div>
+
+      <Box>
+        <p>Linkのサンプル</p>
+        <div>
+          <Link component={RouterLink} to="/">
+            Link to Home (index)
+          </Link>
+          <br />
+          <Link component={RouterLink} to="/product">
+            Link to Product component.
+          </Link>
+          <Button
+            variant="contained"
+            color="primary"
+            component={RouterLink}
+            to="/"
+          >
+            Button to home
+          </Button>
+        </div>
+      </Box>
+
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="*" element={<Product />} />
+      </Routes>
+
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
