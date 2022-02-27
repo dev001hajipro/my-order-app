079c489740f2c506c83bd2dda372230b30dae508
dev001hajipro <dev001hajipro@gmail.com>
2022-02-27 23:13:52 +0900

    add file to move contents to LinkSample.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import './App.css';
-import { AppBar, Box, Button, IconButton, Link, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, CssBaseline, Divider, Drawer, IconButton, Link, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
 
 import MenuIcon from '@mui/icons-material/Menu'
+import InboxIcon from '@mui/icons-material/MoveToInbox'
+import MailIcon from '@mui/icons-material/Mail'
 import { Link as RouterLink, Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Product } from './components/Product';
 import { Order } from './components/Order';
 import { SignIn } from './components/SignIn';
 import { SignUp } from './components/SignUp';
+import { LinkSample } from './components/LinkSample';
+
+const drawerWidth = 240;
 
 function App() {
   return (
-    <div>
-      <AppBar position="static">
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
           <IconButton
             size="large"
@@ -25,41 +33,48 @@ function App() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            注文
+            メンバー管理アプリ
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-      <div>コンポーネントの切り替えがしたい。</div>
 
-      <Box>
-        <p>Linkのサンプル</p>
-        <div>
-          <Link component={RouterLink} to="/">
-            Link to Home (index)
-          </Link>
-          <br />
-          <Link component={RouterLink} to="/product">
-            Link to Product component.
-          </Link>
-          <br />
-          <Link component={RouterLink} to="/signin">
-            ログイン画面
-          </Link>
-          <br />
-          <Link component={RouterLink} to="/signup">
-            ユーザ登録
-          </Link>
-          <br />
-          <Button
-            variant="contained"
-            color="primary"
-            component={RouterLink}
-            to="/"
-          >
-            Button to home
-          </Button>
-        </div>
+      <Drawer
+        variant="permanent"
+        anchor="left"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {
+            ['Inbox', 'Starred'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <Link component={RouterLink} to="/">
+                  Link to Home (index)
+                </Link>
+
+                <ListItemText primary={text} />
+              </ListItem>
+            ))
+          }
+        </List>
+      </Drawer>
+
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3 }}
+      >
+        <Toolbar />
+
+        <LinkSample></LinkSample>
       </Box>
 
       <Routes>
@@ -71,8 +86,7 @@ function App() {
         <Route path="*" element={<Product />} />
       </Routes>
 
-
-    </div>
+    </Box>
   );
 }
 
---
+++ src/components/LinkSample.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, Link } from '@mui/material';
+import { Link as RouterLink, Route, Routes } from 'react-router-dom';
+
+export const LinkSample: React.FC = () => {
+    return (
+        <div>
+            <p>Linkのサンプル</p>
+            <div>
+                <Link component={RouterLink} to="/">
+                    Link to Home (index)
+                </Link>
+                <br />
+                <Link component={RouterLink} to="/product">
+                    Link to Product component.
+                </Link>
+                <br />
+                <Link component={RouterLink} to="/signin">
+                    ログイン画面
+                </Link>
+                <br />
+                <Link component={RouterLink} to="/signup">
+                    ユーザ登録
+                </Link>
+                <br />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    component={RouterLink}
+                    to="/"
+                >
+                    Button to home
+                </Button>
+            </div>
+        </div>
+    )
+}
